003c8724c52c0f93538ec1039e3299f1a72ff633
Ryan Crespo <crespoffi@gmail.com>
2021-11-28 20:54:43 -0500

    finished project

--- index.js
+++ index.js
@@ -1,178 +1,238 @@
-const bullet = [];
-const aliens = [];
+const KEY_RIGHT = 39;
+const KEY_LEFT = 37;
+const KEY_SPACE = 32;
 
+const gameWidth = 800;
+const gameHeight = 600;
 
-  
+const STATE = {
+    xPos: 0,
+    yPos: 0,
+    moveLeft: false,
+    moveRight: false,
+    shoot: false,
+    bullets: [],
+    aliens: [],
+    alienLasers: [],
+    alienWidth: 50,
+    shipWidth: 50,
+    cooldown: 0,
+    numberOfAliens: 16,
+    alienCooldown: 0,
+    gameOver: false
+}
 
-let canvas = document.getElementById('myCanvas');
+function setPosition(element, x, y) {
+    element.style.transform = `translate(${x}px, ${y}px)`;
+}
 
-ctx = canvas.getContext('2d');
+function setSize(element, width) {
+    element.style.width = `${width}px`;
+    element.style.height = 'auto';
+}
 
-
-canvas.width = 1200;
-canvas.height = 607;
-
-// console.log('width', innerWidth);
-// console.log('height', innerHeight);
-
-// class Game {
-//     constructor()
-// }
-
-class Sprite {
-    constructor(image, x, y, width, height, speed) {
-        this.image = image
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.speed = speed
-    }
-
-    draw() {
-        // console.log(this.image, this.y)
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
-    
-
-    loadImage() {
-        const img = new Image();
-        img.src = this.image;
-        // console.log({alien: this})
-        img.addEventListener("load", () => {
-            this.image = img;
-            this.draw();
-        
-        });
+function bound(x) {
+    if(x >= gameWidth - STATE.shipWidth) {
+        STATE.xPos = gameWidth - STATE.shipWidth;
+        return STATE.xPos;
+    } if(x <= 0) {
+        STATE.xPos = 0;
+        return STATE.xPos;
+    } else {
+        return x;
     }
 }
 
-class Ship extends Sprite {
-    constructor() {
-        super("./images/ship.png", canvas.width/2, 525, 50, 50)
+function removeBullet(bullets, bullet1, bullet) {
+    const index = bullets.indexOf(bullet1);
+    bullets.splice(index, 1);
+    container.removeChild(bullet)
+}
+
+function collide(rect1, rect2) {
+    return!(rect2.left > rect1.right || rect2.right < rect1.left || rect2.top > rect1.bottom || rect2.bottom < rect1.top);
+}
+
+//Ship
+function createPlayer(container) {
+    STATE.xPos = gameWidth / 2;
+    STATE.yPos = gameHeight - 50;
+    const player = document.createElement("img");
+    player.src = "images/ship.png";
+    player.className = "player";
+    container.appendChild(player);
+    setPosition(player, STATE.xPos, STATE.yPos);
+    setSize(player, STATE.shipWidth);
+}
+
+function updatePlayer() {
+    if(STATE.moveLeft) {
+        STATE.xPos -= 4;
+    } if(STATE.moveRight) {
+        STATE.xPos += 4;
+    } if(STATE.shoot && STATE.cooldown == 0) {
+        createBullet(container, STATE.xPos - STATE.shipWidth / 2, STATE.yPos);
+        STATE.cooldown = 20;
     }
+    const player = document.querySelector('.player');
+    setPosition(player, bound(STATE.xPos), STATE.yPos - 15);
+    if(STATE.cooldown > 0) {
+        STATE.cooldown -= 0.5;
+    }
+    console.log(STATE.cooldown)
+}
 
+function createBullet(container, x, y) {
+    const bullet = document.createElement('img');
+    bullet.src = 'images/ship-laser.png';
+    bullet.className = 'bullet';
+    container.appendChild(bullet);
+    const bullet1 = {x, y, bullet};
+    STATE.bullets.push(bullet1);
+    setPosition(bullet, x, y);
+}
 
-
-    move(e) {
-        let rightLimit = canvas.width - this.width
-        rightLimit = rightLimit - 25
-        if(this.x < rightLimit && e.key === "ArrowRight") {
-            // console.log(this.speed)
-          this.x += 7;
+function updateBullet(container) {
+    const bullets = STATE.bullets;
+    for(let i = 0; i < bullets.length; i++) {
+        const bullet1 = bullets[i];
+        bullet1.y -= 2;
+        if(bullet1.y < 0) {
+            removeBullet(bullets, bullet1, bullet1.bullet);
         }
-        if(this.x > 30 && e.key === "ArrowLeft") {
-          this.x -= 7;
+        setPosition(bullet1.bullet, bullet1.x, bullet1.y);
+        const bulletRect = bullet1.bullet.getBoundingClientRect();
+        const aliens = STATE.aliens;
+        for(let j = 0; j < aliens.length; j++) {
+            const alien = aliens[j];
+            const alienRect = alien.alien1.getBoundingClientRect();
+            if(collide(alienRect, bulletRect)) {
+                removeBullet(bullets, bullet1, bullet1.bullet);
+                const index = aliens.indexOf(alien);
+                aliens.splice(index, 1);
+                container.removeChild(alien.alien1);
+            }
         }
     }
 }
 
-
-
-// document.addEventListener('keydown', function(e) {
-//     // console.log(e.which);
-//     switch (e.keycode) {
-//         case 37:
-//       ship.moveLeft();
-//       console.log('left', ship);
-//       break;
-//     case 39:
-//       ship.moveRight();
-//       console.log('right', ship);
-//       break;
-//     }
-//     updateCanvas();
-//   });
-
-const ship = new Ship();
-function shipImageLoad() {
-    ship.loadImage();
+function createAlien(container, x, y) {
+    const alien1 = document.createElement('img');
+    alien1.src = 'images/alien.png';
+    alien1.className = 'alien';
+    container.appendChild(alien1);
+    const alienCooldown = Math.floor(Math.random()* 100);
+    const alien = {x, y, alien1, alienCooldown};
+    STATE.aliens.push(alien);
+    setSize(alien1, STATE.alienWidth);
+    setPosition(alien1, x, y);
 }
 
-shipImageLoad();
-
-
-class Alien extends Sprite {
-    constructor(image, x, y, width, height, speed) {
-        super(image, x, y, width, height, speed)
-        
-    }
-    
-    
-}
-
-
-// const alien = new Alien();
-function alienImageLoad() {
-    // alien.loadImage();
-    aliens.forEach(alien => {
-        // console.log({alien})
-        alien.loadImage();
-    })
-}
-
-alienImageLoad();
-
-// class Bullet extends Sprite {
-//     constructor() {
-
-//     }
-// }
-
-
-const generateAliens = () => {
-    let rowX = 0;
-    let colY = 0;
-    for (let row = 0; row < 4; row++) {
-        for (let col = 0; col < 10; col++) {
-            // aliens.push(alien);
-            aliens.push(new Alien("./images/alien.png", rowX, colY, 50, 50, 0))
-            rowX += 70;
+function updateAliens(container) {
+    const dx = Math.cos(Date.now() / 1000) * 40;
+    const dy = Math.sin(Date.now() / 1000) * 30;
+    const aliens = STATE.aliens;
+    for(let i = 0; i < aliens.length; i++) {
+        const alien = aliens[i];
+        let a = alien.x + dx;
+        let b = alien.y + dy;
+        setPosition(alien.alien1, a, b);
+        if(alien.alienCooldown == 0) {
+            createAlienLaser(container, a, b);
+            alien.alienCooldown = Math.floor(Math.random()* 50) + 100;
         }
-        rowX = 0;
-        colY += 70;
+        alien.alienCooldown -= 0.5;  
     }
 }
-generateAliens();
 
-
-const generateBullets = () => {
-    bullet.x = (this.ship.x + this.ship.width) / 2
-    bullet.y = ((this.ship.y + this.ship.width) / 2) + 5
-}
-// generateBullets();
-
-
-function redraw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ship.draw();
-    aliens.forEach(alien => {
-        alien.draw();
-    })
+function createAliens(container) {
+    for(let i = 0; i <= STATE.numberOfAliens / 2; i++) {
+        createAlien(container, i*80, 100);
+    }
+    for(let i = 0; i <= STATE.numberOfAliens / 2; i++) {
+        createAlien(container, i*80, 180);
+    }
 }
 
-// redraw();
-
-function startGame() {
-
-     shipImageLoad();
-     alienImageLoad();
-
-    
-    document.addEventListener('keydown', (e) => { ship.move(e) });
-
-    let spaceInv = setInterval(() => {
-        redraw(spaceInv)
-    }, 33)
+function createAlienLaser(container, x, y) {
+    const alienLaser = document.createElement('img');
+    alienLaser.src = 'images/alien-laser.png';
+    alienLaser.className = 'alien-laser';
+    container.appendChild(alienLaser);
+    const alienLaser1 = {x, y, alienLaser};
+    STATE.alienLasers.push(alienLaser1);
+    setPosition(alienLaser, x, y);
 }
 
-startGame();
+function updateAlienLaser() {
+    const alienLasers = STATE.alienLasers;
+    for(let i = 0; i < alienLasers.length; i++) {
+        const alienLaser1 = alienLasers[i];
+        alienLaser1.y += 2;
+        if(alienLaser1.y > gameHeight - 30) {
+            removeBullet(alienLasers, alienLaser1, alienLaser1.alienLaser);
+        }
+        const alienLaserRect = alienLaser1.alienLaser.getBoundingClientRect();
+        const shipRect = document.querySelector('.player').getBoundingClientRect();
+        if (collide(shipRect, alienLaserRect)) {
+            // console.log('Game Over')
+            STATE.gameOver = true;
+        }
+        setPosition(alienLaser1.alienLaser, alienLaser1.x + STATE.alienWidth / 2, alienLaser1.y + 15);
+    }
+}
+
+//Ship Movement
+function keyPress(event) {
+    if(event.keyCode === KEY_RIGHT) {
+        STATE.moveRight = true;
+        // console.log('right key')
+    } else if(event.keyCode === KEY_LEFT) {
+        STATE.moveLeft = true;
+        // console.log('left key')
+    } else if (event.keyCode === KEY_SPACE) {
+        STATE.shoot = true;
+        // console.log('shoot')
+    }
+}
+
+function keyRelease(event) {
+    if(event.keyCode === KEY_RIGHT) {
+        STATE.moveRight = false;
+    } else if(event.keyCode === KEY_LEFT) {
+        STATE.moveLeft = false;
+    } else if(event.keyCode === KEY_SPACE) {
+        STATE.shoot = false;
+    }
+}
+
+//Game
+function update() {
+    updatePlayer();
+    updateBullet(container);
+    updateAliens(container);
+    updateAlienLaser();
+
+    window.requestAnimationFrame(update);
+
+    if(STATE.gameOver) {
+        document.querySelector('.lose').style.display = 'block';
+    }
+    if(STATE.aliens.length == 0) {
+        document.querySelector('.win').style.display = 'block';
+    }
+}
 
 
-
-// window.onload = () => {
-//     document.getElementById()
-// }
-
+const container = document.querySelector(".main");
+createPlayer(container);
+createAliens(container);
 
 
+// Event Listeners
+window.addEventListener('keydown', keyPress);
+window.addEventListener('keyup', keyRelease);
+
+
+//Runs Game
+update();
